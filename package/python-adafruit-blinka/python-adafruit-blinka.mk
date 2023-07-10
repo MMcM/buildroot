cade264a81cea73b14e3766adbe1117bb0328e30
Mike McMahon <mmcm@comcast.net>
2023-07-09 18:53:46 -0700

    Tweaks to avoid setuptools_scm

--- package/python-adafruit-blinka/python-adafruit-blinka.mk
+++ package/python-adafruit-blinka/python-adafruit-blinka.mk
@@ -4,11 +4,13 @@
 #
 ################################################################################
 
-PYTHON_ADAFRUIT_BLINKA_VERSION = 8.20.0
-PYTHON_ADAFRUIT_BLINKA_SOURCE = Adafruit-Blinka-$(PYTHON_ADAFRUIT_BLINKA_VERSION).tar.gz
-PYTHON_ADAFRUIT_BLINKA_SITE = https://files.pythonhosted.org/packages/4d/42/8b0798116e36b6d28fdfc1d3d86c8672735884378d04c8c06674abba0d00
+PYTHON_ADAFRUIT_BLINKA_VERSION = 0dc9eae02e9fd3f5fc7d6300c0f4cfcd1ec73135
+PYTHON_ADAFRUIT_BLINKA_SITE = $(call github,MMcM,Adafruit_Blinka,$(PYTHON_ADAFRUIT_BLINKA_VERSION))
 PYTHON_ADAFRUIT_BLINKA_SETUP_TYPE = setuptools
 PYTHON_ADAFRUIT_BLINKA_LICENSE = MIT
 PYTHON_ADAFRUIT_BLINKA_LICENSE_FILES = LICENSE
+PYTHON_ADAFRUIT_BLINKA_BIN_ARCH_EXCLUDE = \
+  /usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/adafruit_blinka/microcontroller/amlogic/meson_g12_common/pulseio/libgpiod_pulsein \
+  /usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/adafruit_blinka/microcontroller/bcm283x/pulseio/libgpiod_pulsein64
 
 $(eval $(python-package))
