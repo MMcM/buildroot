dbf979cb0c001dbccf3de965177d7ff6d531a6ca
Mike McMahon <mmcm@comcast.net>
2026-05-02 18:27:32 -0700

    Update Adafruit Python package versions.

--- package/python-adafruit-blinka/python-adafruit-blinka.mk
+++ package/python-adafruit-blinka/python-adafruit-blinka.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-PYTHON_ADAFRUIT_BLINKA_VERSION = 0dc9eae02e9fd3f5fc7d6300c0f4cfcd1ec73135
+PYTHON_ADAFRUIT_BLINKA_VERSION = 46615d10cab436c89f75a28ac3cf6da9743ad598
 PYTHON_ADAFRUIT_BLINKA_SITE = $(call github,MMcM,Adafruit_Blinka,$(PYTHON_ADAFRUIT_BLINKA_VERSION))
 PYTHON_ADAFRUIT_BLINKA_SETUP_TYPE = setuptools
 PYTHON_ADAFRUIT_BLINKA_LICENSE = MIT
 PYTHON_ADAFRUIT_BLINKA_LICENSE_FILES = LICENSE
 PYTHON_ADAFRUIT_BLINKA_BIN_ARCH_EXCLUDE = \
-  /usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/adafruit_blinka/microcontroller/amlogic/meson_g12_common/pulseio/libgpiod_pulsein \
-  /usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/adafruit_blinka/microcontroller/bcm283x/pulseio/libgpiod_pulsein64
+  /usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/adafruit_blinka/microcontroller/amlogic/meson_g12_common/pulseio/ \
+  /usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/adafruit_blinka/microcontroller/bcm283x/pulseio/
 
 $(eval $(python-package))
